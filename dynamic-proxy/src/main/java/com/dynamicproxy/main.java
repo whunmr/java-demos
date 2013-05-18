fd0c33ac41a1e153fd51853b1575bad21fd63809
Hongmin Wang <whunmr@gmail.com>
2013-05-18 17:09:46 +0800

    add dynamic-proxy demo

---
+++ dynamic-proxy/src/main/java/com/dynamicproxy/main.java
@@ -0,0 +1,50 @@
+package com.dynamicproxy;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+interface IServer {
+    public void send(String msg);
+    public String receive();
+}
+
+class ServerImpl implements IServer {
+    @Override
+    public void send(String msg) {
+        System.out.println("ServerImpl send: \"" + msg + "\"");
+    }
+
+    @Override
+    public String receive() {
+        System.out.println("ServerImpl receive.");
+        return "received message";
+    }
+}
+
+class ServerInvocationHandler implements InvocationHandler {
+    private final ServerImpl server;
+
+    public ServerInvocationHandler(ServerImpl server) {
+        this.server = server;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        System.out.println("calling start---> :" + method.getName());
+        Object result = method.invoke(server, args);
+        System.out.println("calling end---> :" + method.getName());
+
+        return result;
+    }
+}
+
+public class main {
+    public static void main(String args[]) {
+        IServer server = (IServer) Proxy.newProxyInstance(main.class.getClassLoader(),
+                new Class[]{IServer.class},
+                new ServerInvocationHandler(new ServerImpl()));
+
+        server.send("hello");
+    }
+}
